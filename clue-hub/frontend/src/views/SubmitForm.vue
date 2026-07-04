3fb374ad7f9ed38c793b8a3e20526e87f7f366f8
1228lzh <1412158844@qq.com>
2026-07-05 00:14:54 +0800

    style: add required asterisk to all four submitter fields

--- clue-hub/frontend/src/views/SubmitForm.vue
+++ clue-hub/frontend/src/views/SubmitForm.vue
@@ -16,10 +16,10 @@
     <div class="step-content">
       <!-- 第一步：谁报的 -->
       <template v-if="currentStep === 0">
-        <van-field v-model="form.wecomUserId" label="报备人工号" readonly class="readonly-field" />
-        <van-field v-model="form.reporterName" label="报备人姓名" readonly class="readonly-field" />
-        <van-field v-model="form.reporterDept" label="部门" placeholder="请输入部门" />
-        <van-field v-model="form.reporterContact" label="联系方式" placeholder="请输入联系方式" />
+        <van-field v-model="form.wecomUserId" label="报备人工号" readonly class="readonly-field" required />
+        <van-field v-model="form.reporterName" label="报备人姓名" readonly class="readonly-field" required />
+        <van-field v-model="form.reporterDept" label="部门" placeholder="请输入部门" required />
+        <van-field v-model="form.reporterContact" label="联系方式" placeholder="请输入联系方式" required />
       </template>
 
       <!-- 第二步：什么线索 -->
